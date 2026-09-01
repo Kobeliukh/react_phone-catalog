450a5364ea92d9eb8d19faa0395549e6b6fe4f28
Vladyslav <vkobeluh@gmail.com>
2026-09-01 11:59:03 +0300

    feat: implement favorites page

--- src/modules/FavoritesPage/FavoritesPage.tsx
+++ src/modules/FavoritesPage/FavoritesPage.tsx
@@ -1,5 +1,26 @@
-// import styles from './FavoritesPage.module.scss';
+import { CategoryPage } from '@/shared/components/CategoryPage';
+import { useFavoritesState } from '@/shared/hooks/useFavoritesState';
+import { useProductsContext } from '@/shared/hooks/useProductsContext';
+import { useMemo } from 'react';
 
 export const FavoritesPage = () => {
-  return <h1>Favorites Page</h1>;
+  const productsState = useProductsContext();
+  const favoritesState = useFavoritesState();
+
+  const favorites = useMemo(
+    () =>
+      productsState.products.filter(product =>
+        favoritesState.includes(product.id),
+      ),
+    [favoritesState, productsState.products],
+  );
+
+  return (
+    <CategoryPage
+      title="Favourites"
+      products={favorites}
+      hasFilters={false}
+      productsName="items"
+    />
+  );
 };
