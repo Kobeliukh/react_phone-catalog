cd27436ecbac67be22ac16b20b2709d875873b0b
Vladyslav <vkobeluh@gmail.com>
2026-09-02 07:18:55 +0300

    feat(ui): extend IconButton with cart variants and border toggle

--- src/shared/components/IconButton/IconButton.tsx
+++ src/shared/components/IconButton/IconButton.tsx
@@ -1,19 +1,33 @@
+/* eslint-disable @typescript-eslint/indent */
+
 import styles from './IconButton.module.scss';
 import classNames from 'classnames';
 
 interface BaseProps {
   className?: string;
   disabled?: boolean;
+  hasBorder?: boolean;
   onClick?: () => void;
 }
 
 type Props =
   | (BaseProps & { variant: 'pagination'; number: number; selected?: boolean })
   | (BaseProps & { variant: 'arrow'; selected?: never; rotate?: number })
-  | (BaseProps & { variant: 'favorite'; selected?: boolean; rotate?: never });
+  | (BaseProps & { variant: 'favorite'; selected?: boolean; rotate?: never })
+  | (BaseProps & {
+      variant: 'close' | 'plus' | 'minus';
+      selected?: never;
+      rotate?: never;
+    });
 
 export const IconButton = (props: Props) => {
-  const { className, variant, disabled = false, onClick } = props;
+  const {
+    className,
+    variant,
+    disabled = false,
+    onClick,
+    hasBorder = true,
+  } = props;
 
   return (
     <button
@@ -24,6 +38,7 @@ export const IconButton = (props: Props) => {
         styles[variant],
         {
           [styles.selected]: props.selected,
+          [styles.hasBorder]: hasBorder,
         },
         className,
       )}
