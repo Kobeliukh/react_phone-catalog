27a432e66f037778338db19d9ecd1e1035b7fedf
Vladyslav <vkobeluh@gmail.com>
2026-09-02 15:59:49 +0300

    feat: add className prop to Button

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -3,8 +3,9 @@ import styles from './Button.module.scss';
 
 interface Props {
   text: string;
-  activeText: string;
+  activeText?: string;
   selected?: boolean;
+  className?: string;
   onClick?: () => void;
 }
 
@@ -12,12 +13,17 @@ export const Button = ({
   text,
   activeText,
   selected = false,
+  className,
   onClick,
 }: Props) => {
   return (
     <button
       type="button"
-      className={classNames(styles.button, { [styles.isSelected]: selected })}
+      className={classNames(
+        styles.button,
+        { [styles.isSelected]: selected },
+        className,
+      )}
       onClick={onClick}
     >
       <span className={styles.text}>{selected ? activeText : text}</span>
