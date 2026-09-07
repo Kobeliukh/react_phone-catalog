e717fc8e096b52290621b7ac314b084b8be58f7f
Vladyslav <vkobeluh@gmail.com>
2026-09-07 14:36:02 +0300

    feat(api): add getCategory function

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,13 @@
-import { Product } from '@/types/product';
+import { Categories } from '@/types/Categories';
+import { Product } from '@/types/Product';
+import { ProductDetails } from '@/types/ProductDetails';
 
 export const getProducts = async (): Promise<Product[]> => {
   return (await fetch('/api/products.json')).json();
+};
+
+export const getCategory = async (
+  category: Categories,
+): Promise<ProductDetails[]> => {
+  return (await fetch(`/api/${category}.json`)).json();
 };
