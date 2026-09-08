fdad540f796fa1be9e42ea4158de77a4010455bf
Vladyslav <vkobeluh@gmail.com>
2026-09-08 12:51:57 +0300

    feat(category-page): add skeleton loading state support

--- src/shared/components/CategoryPage/CategoryPage.tsx
+++ src/shared/components/CategoryPage/CategoryPage.tsx
@@ -4,29 +4,44 @@ import styles from './CategoryPage.module.scss';
 import { Breadcrumbs } from '../Breadcrumbs';
 import { PageLayout } from '../PageLayout';
 import { ProductsList } from '../ProductsList';
+// eslint-disable-next-line max-len
+import { ProductsListSkeleton } from '../ProductsList/components/ProductsListSkeleton';
 
 interface Props {
   products: Product[];
   title: string;
   emptyMessage: string;
+  isLoading?: boolean;
 }
 
-export const CategoryPage = ({ products, title, emptyMessage }: Props) => {
+export const CategoryPage = ({
+  products,
+  title,
+  emptyMessage,
+  isLoading = false,
+}: Props) => {
   const headerElement = (
     <PageHeader
       title={title}
       productsCount={products.length}
       productsName="models"
+      isLoading={isLoading}
     />
   );
 
+  let content: React.ReactNode = null;
+
+  if (isLoading) {
+    content = <ProductsListSkeleton />;
+  } else if (products.length > 0) {
+    content = <ProductsList products={products} />;
+  } else {
+    content = <span className={styles.emptyMessage}>{emptyMessage}</span>;
+  }
+
   return (
     <PageLayout topNav={<Breadcrumbs />} header={headerElement}>
-      {products.length > 0 ? (
-        <ProductsList products={products} />
-      ) : (
-        <span className={styles.emptyMessage}>{emptyMessage}</span>
-      )}
+      {content}
     </PageLayout>
   );
 };
--- src/shared/components/PageHeader/PageHeader.tsx
+++ src/shared/components/PageHeader/PageHeader.tsx
@@ -1,25 +1,37 @@
+import { toRem } from '@/shared/utils/toRem';
+import { Skeleton } from '../Skeleton';
 import styles from './PageHeader.module.scss';
 
 interface Props {
   title: string;
   productsCount: number;
   productsName?: string;
+  isLoading?: boolean;
 }
 
 export const PageHeader = ({
   title,
   productsCount,
   productsName = 'items',
+  isLoading = false,
 }: Props) => {
+  let count: React.ReactNode = null;
+
+  if (isLoading) {
+    count = <Skeleton width={toRem(100)} height={toRem(21)} />;
+  } else if (productsCount > 0) {
+    count = (
+      <p className={styles.modelsCount}>
+        {productsCount} {productsName}
+      </p>
+    );
+  }
+
   return (
     <div className={styles.pageHeader}>
       <h1 className={styles.title}>{title}</h1>
 
-      {productsCount > 0 && (
-        <p className={styles.modelsCount}>
-          {productsCount} {productsName}
-        </p>
-      )}
+      {count}
     </div>
   );
 };
