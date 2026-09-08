96917c981bbe014e7b1520a16491bd865dc61f2a
Vladyslav <vkobeluh@gmail.com>
2026-09-08 13:14:18 +0300

    feat(product-list-skeleton): add hasFilters and hasPagination props

--- src/shared/components/ProductsList/components/ProductsListSkeleton/ProductsListSkeleton.tsx
+++ src/shared/components/ProductsList/components/ProductsListSkeleton/ProductsListSkeleton.tsx
@@ -7,26 +7,36 @@ import classNames from 'classnames';
 import { usePaginationParams } from '../../hooks/usePaginationParams';
 import { PAGINATION_BUTTONS } from '../../constants/pagination';
 
+interface Props {
+  hasFilters?: boolean;
+  hasPagination?: boolean;
+}
+
 const PRODUCT_CARDS_COUNT = 16;
 
-export const ProductsListSkeleton = () => {
+export const ProductsListSkeleton = ({
+  hasFilters = true,
+  hasPagination = true,
+}: Props) => {
   const { perPage } = usePaginationParams();
 
   const skeletonsCount = +perPage || PRODUCT_CARDS_COUNT;
 
   return (
     <div className={styles.productsListSkeleton}>
-      <div className={styles.controls}>
-        <div className={classNames(styles.dropdown, styles.sortBy)}>
-          <Skeleton width={toRem(48)} height={toRem(15)} />
-          <Skeleton width="100%" height={toRem(42)} />
-        </div>
+      {hasFilters && (
+        <div className={styles.controls}>
+          <div className={classNames(styles.dropdown, styles.sortBy)}>
+            <Skeleton width={toRem(48)} height={toRem(15)} />
+            <Skeleton width="100%" height={toRem(42)} />
+          </div>
 
-        <div className={classNames(styles.dropdown, styles.itemsPerPage)}>
-          <Skeleton width={toRem(90)} height={toRem(15)} />
-          <Skeleton width="100%" height={toRem(42)} />
+          <div className={classNames(styles.dropdown, styles.itemsPerPage)}>
+            <Skeleton width={toRem(90)} height={toRem(15)} />
+            <Skeleton width="100%" height={toRem(42)} />
+          </div>
         </div>
-      </div>
+      )}
 
       <div className={styles.content}>
         <div className={styles.list}>
@@ -35,23 +45,25 @@ export const ProductsListSkeleton = () => {
           ))}
         </div>
 
-        <div className={styles.pagination}>
-          <div className={styles.IconButton}>
-            <Skeleton width={toRem(32)} height={toRem(32)} />
-          </div>
+        {hasPagination && (
+          <div className={styles.pagination}>
+            <div className={styles.IconButton}>
+              <Skeleton width={toRem(32)} height={toRem(32)} />
+            </div>
 
-          <div className={styles.pages}>
-            {Array.from({ length: PAGINATION_BUTTONS }, (_, index) => (
-              <div key={index} className={styles.IconButton}>
-                <Skeleton width={toRem(32)} height={toRem(32)} />
-              </div>
-            ))}
-          </div>
+            <div className={styles.pages}>
+              {Array.from({ length: PAGINATION_BUTTONS }, (_, index) => (
+                <div key={index} className={styles.IconButton}>
+                  <Skeleton width={toRem(32)} height={toRem(32)} />
+                </div>
+              ))}
+            </div>
 
-          <div className={styles.IconButton}>
-            <Skeleton width={toRem(32)} height={toRem(32)} />
+            <div className={styles.IconButton}>
+              <Skeleton width={toRem(32)} height={toRem(32)} />
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
