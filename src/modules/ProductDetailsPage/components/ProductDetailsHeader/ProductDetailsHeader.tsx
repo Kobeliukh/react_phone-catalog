8fb483674c71ee1d72125f28d3de39da05a22512
Vladyslav <vkobeluh@gmail.com>
2026-09-09 13:09:52 +0300

    feat(product-details-header): add skeleton loading state support

--- src/modules/ProductDetailsPage/components/ProductDetailsHeader/ProductDetailsHeader.tsx
+++ src/modules/ProductDetailsPage/components/ProductDetailsHeader/ProductDetailsHeader.tsx
@@ -1,16 +1,24 @@
 import { BackButton } from '@/shared/components/BackButton';
 import styles from './ProductDetailsHeader.module.scss';
+import { Skeleton } from '@/shared/components/Skeleton';
 
 interface Props {
   title: string;
+  isLoading?: boolean;
 }
 
-export const ProductDetailsHeader = ({ title }: Props) => {
+export const ProductDetailsHeader = ({ title, isLoading = false }: Props) => {
   return (
     <div className={styles.productDetailsHeader}>
       <BackButton />
 
-      <h1 className={styles.title}>{title}</h1>
+      {isLoading ? (
+        <div className={styles.titleSkeleton}>
+          <Skeleton width="100%" height="100%" />
+        </div>
+      ) : (
+        <h1 className={styles.title}>{title}</h1>
+      )}
     </div>
   );
 };
